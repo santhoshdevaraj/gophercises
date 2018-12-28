e915234bc992a64b8e55ff267b77421dacd730a7
Santhosh Devaraj <skdk@justin.tv>
2018-12-28 12:09:23 -0800

    merged main and handler

--- url-shortener/shortener.go
+++ url-shortener/shortener.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func mapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
+	return nil
+}
+
+func yamlHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	return nil, nil
+}
 
 func main() {
-	fmt.Println("Hello Url shortener")
+	mux := defaultMux()
+
+	pathsToUrls := map[string]string{
+		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
+		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
+	}
+	m := mapHandler(pathsToUrls, mux)
+
+	yaml :=
+		`
+		- path: /urlshort
+		url: https://github.com/gophercises/urlshort
+		- path: /urlshort-final
+		url: https://github.com/gophercises/urlshort/tree/solution
+		`
+	y, err := yamlHandler([]byte(yaml), m)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Starting the server on :8080")
+	http.ListenAndServe(":8080", y)
+}
+
+func defaultMux() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", hello)
+	return mux
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, world!")
 }
