59bc2ea72bc6d8467a2e6f1c815dfc8acbbfc6a1
Santhosh Devaraj <skdk@justin.tv>
2018-12-30 17:18:51 -0800

    completed code

--- url-shortener/shortener.go
+++ url-shortener/shortener.go
@@ -3,14 +3,38 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	yaml "gopkg.in/yaml.v2"
 )
 
+type pathUrl struct {
+	Path string `yaml:"path"`
+	URL  string `yaml:"url"`
+}
+
 func mapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
-	return nil
+	return func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path
+		if path != "/favicon.ico" {
+			if val, ok := pathsToUrls[path]; ok {
+				http.Redirect(w, r, val, 301)
+			}
+			fallback.ServeHTTP(w, r)
+		}
+	}
 }
 
 func yamlHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
-	return nil, nil
+	var pathUrls []pathUrl
+	err := yaml.Unmarshal(yml, &pathUrls)
+	if err != nil {
+		return nil, err
+	}
+	pathsToUrls := make(map[string]string)
+	for _, pu := range pathUrls {
+		pathsToUrls[pu.Path] = pu.URL
+	}
+	return mapHandler(pathsToUrls, fallback), nil
 }
 
 func main() {
@@ -22,13 +46,12 @@ func main() {
 	}
 	m := mapHandler(pathsToUrls, mux)
 
-	yaml :=
-		`
-		- path: /urlshort
-		url: https://github.com/gophercises/urlshort
-		- path: /urlshort-final
-		url: https://github.com/gophercises/urlshort/tree/solution
-		`
+	yaml := `
+- path: /urlshort
+  url: https://github.com/gophercises/urlshort
+- path: /urlshort-final
+  url: https://github.com/gophercises/urlshort/tree/solution
+`
 	y, err := yamlHandler([]byte(yaml), m)
 	if err != nil {
 		panic(err)
